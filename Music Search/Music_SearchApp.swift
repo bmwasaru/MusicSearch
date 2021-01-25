1439f38ff7bdf7f4a674b5f01edb184b626115d5
britone mwasaru <bmwasaru@gmail.com>
2021-01-25 05:41:04 +0300

    Initial Commit

---
+++ Music Search/Music_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_SearchApp.swift
+//  Music Search
+//
+//  Created by Britone Mwasaru on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Music_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
